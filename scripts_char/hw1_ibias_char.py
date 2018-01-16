a8dc421bbaad5ce4af17801dd53eb070daffe29d
Eric Chang <pkerichang@gmail.com>
2018-01-16 11:20:10 -0800

    update characterization script.

--- scripts_char/hw1_ibias_char.py
+++ scripts_char/hw1_ibias_char.py
@@ -20,8 +20,8 @@ impl_lib = 'AAAFOO_hw1_tb_ibias'
 data_dir = os.path.join('data', 'hw1')
 # sweep values
 mos_list = ['nch', 'pch']
-thres_list = ['svt']
-lch_list = [45e-9, ]
+thres_list = ['lvt']
+lch_list = [45e-9]
 
 # create data directory if it doesn't exist
 os.makedirs(data_dir, exist_ok=True)
@@ -40,6 +40,10 @@ def characterize(prj):
             for lch in lch_list:
                 # new generated testbench name
                 tb_name = get_tb_name(mos_type, thres, lch)
+                fname = os.path.join(data_dir, '%s.data' % tb_name)
+                if os.path.isfile(fname):
+                    print('File %s exists, skipping.')
+                    continue
                 print('Creating testbench %s...' % tb_name)
                 # create schematic generator
                 tb_sch = prj.create_design_module(tb_lib, tb_cell)
@@ -49,13 +53,13 @@ def characterize(prj):
                 tb_obj = prj.configure_testbench(impl_lib, tb_name)
                 # make sure vgs/vds has correct sign
                 if mos_type == 'nch':
-                    tb_obj.set_parameter('vgs_start', 0)
+                    tb_obj.set_parameter('vgs_start', 0.3)
                     tb_obj.set_parameter('vgs_stop', 1.2)
-                    tb_obj.set_sweep_parameter('vds', start=0.0, stop=1.2, step=0.05)
+                    tb_obj.set_sweep_parameter('vds', start=0.0, stop=1.2, step=0.1)
                 else:
                     tb_obj.set_parameter('vgs_start', -1.2)
-                    tb_obj.set_parameter('vgs_stop', 0.0)
-                    tb_obj.set_sweep_parameter('vds', start=-1.2, stop=0.0, step=0.05)
+                    tb_obj.set_parameter('vgs_stop', -0.3)
+                    tb_obj.set_sweep_parameter('vds', start=-1.2, stop=0.0, step=0.1)
                 # update testbench changes and run simulation
                 tb_obj.update_testbench()
                 print('Simulating testbench %s...' % tb_name)
@@ -64,7 +68,7 @@ def characterize(prj):
                 print('Simulation done, saving results')
                 results = load_sim_results(save_dir)
                 # save simulation results to data directory
-                save_sim_results(results, os.path.join(data_dir, '%s.data' % tb_name))
+                save_sim_results(results, fname)
 
     print('Characterization done.')
 
@@ -86,12 +90,11 @@ def print_data_info():
     # show how to get sweep parameter order
     print('sweep_params:')
     pprint.pprint(results['sweep_params'])
-    
 
     
 def plot_data():
     # output data name
-    output = 'ibias'
+    output_names = ['ibias', ]
 
     # plot all ibias data
     fig_idx = 1
@@ -102,35 +105,40 @@ def plot_data():
                 tb_name = get_tb_name(mos_type, thres, lch)
                 results = load_sim_file(os.path.join(data_dir, '%s.data' % tb_name))
                 # plot data
-                plot_2d_data(mos_type, results, 'ibias', fig_idx)
-                fig_idx += 1
+                for output in output_names:
+                    plot_2d_data(mos_type, results, output, fig_idx)
+                    fig_idx += 1
 
     plt.show()
 
 
 def plot_2d_data(mos_type, results, output, fig_idx):
-    # get sweep variables name and order
-    xvar_list = results['sweep_params'][output]
-    # outer sweep variable 1D array
-    xvec = results[xvar_list[0]]
-    # inner sweep variable 1D array
-    yvec = results[xvar_list[1]]
-    # output 2D array
-    zmat = results[output]
-    # change 1D sweep array into 2D array with same size as results
-    xmat, ymat = np.meshgrid(xvec, yvec, indexing='ij', copy=False)
+    if output in results:
+        # get sweep variables name and order
+        xvar_list = results['sweep_params'][output]
+        # outer sweep variable 1D array
+        xvec = results[xvar_list[0]]
+        # inner sweep variable 1D array
+        yvec = results[xvar_list[1]]
+        # output 2D array
+        zmat = results[output]
+        # change 1D sweep array into 2D array with same size as results
+        xmat, ymat = np.meshgrid(xvec, yvec, indexing='ij', copy=False)
 
-    if mos_type == 'pch':
-        # make sure bias current is positive
-        zmat *= -1
+        print(output, np.max(zmat))
+        if output == 'ibias' and mos_type == 'pch':
+            # make sure bias current is positive
+            zmat = -zmat
     
-    # plot 2D array output
-    fig = plt.figure(fig_idx)
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(xmat, ymat, zmat, rstride=1, cstride=2, linewidth=0, cmap=cm.cubehelix)
-    ax.set_xlabel(xvar_list[0])
-    ax.set_ylabel(xvar_list[1])
-    ax.set_zlabel(output)
+        # plot 2D array output
+        fig = plt.figure(fig_idx)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(xmat, ymat, zmat, rstride=1, cstride=2, linewidth=0, cmap=cm.cubehelix)
+        ax.set_xlabel(xvar_list[0])
+        ax.set_ylabel(xvar_list[1])
+        ax.set_zlabel(output)
+    else:
+        print('output %s not found' % output)
     
 
 if __name__ == '__main__':
